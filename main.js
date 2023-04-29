3641820f04bbcda0d0148e6f500ca2e33eb511a7
Ngoc Nguyen <nnpk0410@gmail.com>
2023-04-29 11:55:54 -0500

    adds form to add new employee in index.html

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const employees = document.getElementById("employee-list");
 
-    fetchEmployees();
+    fetchEmployees(); // show employee info
 
+    
     function fetchEmployees(){
         fetch("http://localhost:3000/employees")
         .then(response => response.json())
         .then(employees => employees.forEach(employee => displayEmployeeInfo(employee)));
-    }   
+    }  
+    
+    
     function displayEmployeeInfo(employee) {
         let employeeInfo = document.createElement("div");
         let nameAndLogin = document.createElement("li");
