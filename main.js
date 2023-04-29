b14bbcc10f7eae4ad109e96a6da54a0dac463d27
Ngoc Nguyen <nnpk0410@gmail.com>
2023-04-29 15:38:26 -0500

    change employee display in list to display in table in both index.html and main.js

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const employees = document.getElementById("employee-list");
+  const employees = document.getElementById("employee-table");
 
-    fetchEmployees(); // show employee info
+  fetchEmployees(); // show employee info
 
-    
-    function fetchEmployees(){
-        fetch("http://localhost:3000/employees")
-        .then(response => response.json())
-        .then(employees => employees.forEach(employee => displayEmployeeInfo(employee)));
-    }  
-    
-    
-    function displayEmployeeInfo(employee) {
-        let employeeInfo = document.createElement("div");
-        let nameAndLogin = document.createElement("li");
-        let skill = document.createElement("span");
-        
-        nameAndLogin.textContent = `Name: ${employee.name} / Login: ${employee.login}`;
-        skill.textContent = `Skill: ${employee.skill}`;
-        
-        employeeInfo.append(nameAndLogin, skill);
-        employees.appendChild(employeeInfo);
-    }
-})
+  function fetchEmployees() {
+    fetch("http://localhost:3000/employees")
+      .then((response) => response.json())
+      .then((employees) =>
+        employees.forEach((employee) => displayEmployeeInfo(employee))
+      );
+  }
+
+  function displayEmployeeInfo(employee) {
+    let employeeRow = document.createElement("tr");
+    let nameCell = document.createElement("td");
+    let loginCell = document.createElement("td");
+    let skillCell = document.createElement("td");
+
+    nameCell.textContent = employee.name;
+    loginCell.textContent = employee.login;
+    skillCell.textContent = employee.skill;
+
+    employeeRow.append(nameCell, loginCell, skillCell);
+    employees.appendChild(employeeRow);
+  }
+});
