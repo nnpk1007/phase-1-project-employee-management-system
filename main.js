f958196ab77a268a604d815c42af7190f14610e1
Ngoc Nguyen <nnpk0410@gmail.com>
2023-04-29 11:25:57 -0500

    adds header, section employees in index.html

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-console.log("JS is working")
+document.addEventListener("DOMContentLoaded", () => {
+    const employees = document.getElementById("employee-list");
 
-fetch("http://localhost:3000/employees")
-.then(response => response.json())
-.then(data => console.log(data))
+    fetchEmployees();
 
+    function fetchEmployees(){
+        fetch("http://localhost:3000/employees")
+        .then(response => response.json())
+        .then(employees => employees.forEach(employee => displayEmployeeInfo(employee)));
+    }   
+    function displayEmployeeInfo(employee) {
+        let employeeInfo = document.createElement("div");
+        let nameAndLogin = document.createElement("li");
+        let skill = document.createElement("span");
+        
+        nameAndLogin.textContent = `Name: ${employee.name} / Login: ${employee.login}`;
+        skill.textContent = `Skill: ${employee.skill}`;
+        
+        employeeInfo.append(nameAndLogin, skill);
+        employees.appendChild(employeeInfo);
+    }
+})
