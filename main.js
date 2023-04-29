61ebc5661083688501108fa2dca30b3a54cad150
Ngoc Nguyen <nnpk0410@gmail.com>
2023-04-29 16:59:28 -0500

    add function addEmployee

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const employees = document.getElementById("employee-table");
 
-  fetchEmployees(); // show employee info
+  fetchEmployees(); // show employee info when page is load
+  addEmployee(); // add new employee and save to db.json
 
   function fetchEmployees() {
     fetch("http://localhost:3000/employees")
       .then((response) => response.json())
       .then((employees) =>
-        employees.forEach((employee) => displayEmployeeInfo(employee))
+        employees.forEach((employee) => createEmployeeElement(employee))
       );
   }
-
-  function displayEmployeeInfo(employee) {
+  
+  function createEmployeeElement(employee) {
     let employeeRow = document.createElement("tr");
     let nameCell = document.createElement("td");
     let loginCell = document.createElement("td");
@@ -23,5 +24,38 @@ document.addEventListener("DOMContentLoaded", () => {
 
     employeeRow.append(nameCell, loginCell, skillCell);
     employees.appendChild(employeeRow);
+  }
+
+  function addEmployee() {
+    const addEmployeeForm = document.getElementById("add-employee-form");
+
+    addEmployeeForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let employeeName = document.getElementById("employee-name");
+      let employeeLogin = document.getElementById("employee-login");
+      let employeeSkill = document.getElementById("employee-skill");
+
+      let newEmployee = {
+        name: employeeName.value,
+        login: employeeLogin.value,
+        skill: employeeSkill.value,
+      };
+      saveEmployee(newEmployee);
+
+      addEmployeeForm.reset();
+    });
+  }
+
+  function saveEmployee(newEmployee) {
+    fetch("http://localhost:3000/employees", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(newEmployee),
+    })
+      .then((response) => response.json())
+      .then((employee) => createEmployeeElement(employee));
   }
 });
