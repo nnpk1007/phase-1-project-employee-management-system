b33da6a0c7851344344e56d257c1b38d404f42d4
Ngoc Nguyen <nnpk0410@gmail.com>
2023-05-11 07:55:56 -0500

    create index.js

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const employeesTable = document.getElementById("employee-table");
   const rows = employeesTable.getElementsByTagName("tr");
